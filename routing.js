82e982b8e956c7f530c31c7e30f7d83cd5992433
Vicky Pramanik <vicky.pramanik.1991@gmail.com>
2023-05-24 20:43:32 -0700

    change password route added

--- routing.js
+++ routing.js
@@ -1,5 +1,6 @@
 module.exports.route = (app) => {
     app.use('/api/registration', require('./controller/user/registration/register.route').register);
     app.use('/api/login', require('./controller/login/login.route').login);
+    app.use('/api/change-password', require('./controller/change_password/password.route').changePassword);
     app.use('/api/user/bank-details', require('./controller/user/bankdetail/bankdetail.route').bankdetail);
 }
