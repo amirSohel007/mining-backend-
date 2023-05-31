8ae88878122dcc56d86a873515a68432493aea7d
Vicky Pramanik <vicky.pramanik.1991@gmail.com>
2023-05-31 18:59:13 +0530

    update user, update bank and send fund api fixed

--- controller/user/user.service.js
+++ controller/user/user.service.js
@@ -45,6 +45,28 @@ async function getUserInfo (user_id) {
     }
 }
 
+async function getUser (query) {
+    if (query) {
+        try {
+            const result = await userSchema.findOne(query, { token: 0, password: 0 });
+            if (result) {
+                return result;
+            }
+            return { 
+                message: 'record not found'
+            }
+        } catch(error) {
+            console.log('GET_USER_DETAILS : ', error);
+            throw {
+                status: error.status || 500,
+                message: error
+            }
+        }
+    } else {
+        return null;
+    }
+}
+
 async function getUserAndDownlineTeam (user_id) {
     try {
         let user = await userSchema.findOne({ _id: user_id }, '_id full_name my_reffer_code email')
@@ -79,6 +101,28 @@ async function getUserAndDownlineTeam (user_id) {
     }
 }
 
+async function getUserAndDirectTeam (user_id) {
+    try {
+        let user = await userSchema.findOne({ _id: user_id }, '_id full_name my_reffer_code email')
+        .populate({ 
+            path: 'downline_team', model: 'user', select: '_id full_name' 
+        }).lean().exec();
+
+        if (user) {
+            const team = getLevel(user.downline_team, 1);
+            console.log('DOWNLINE_TEAM_LEVEL : ', team);
+            user.downline_team = team;
+            return user;
+        } else {
+            return { 
+                message: 'record not found'
+            }
+        }
+    } catch (error) {
+        console.log('GET_USERS_DOWNLINE_TEAM_ERROR : ', error);
+    }
+}
+
 function getLevel (arr, level = 1) {
     if (arr && arr.length > 0) {
         for (let i = 0; i < arr.length; i++) {
@@ -93,5 +137,7 @@ module.exports = {
     getUserDetailsWithPopulatedData,
     updateUserDetails,
     getUserInfo,
-    getUserAndDownlineTeam 
+    getUserAndDownlineTeam,
+    getUser,
+    getUserAndDirectTeam
 };
