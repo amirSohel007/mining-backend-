aa7de4cc909eb4a628a517964d2cfe310166c652
Vicky Pramanik <vicky.pramanik.1991@gmail.com>
2023-07-10 13:41:01 +0530

    level income api added

--- controller/income/income.route.js
+++ controller/income/income.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { withdrawlIncome, creditIncome, getIncome, getIncomeTransaction } = require('./income.service');
+const { withdrawlIncome, creditIncome, getIncome, getIncomeTransaction, getLevelIncome } = require('./income.service');
 const responseService = require('../../response/response.handler');
 
 app.post('/withdrawal', async (req, res) => {
@@ -59,6 +59,22 @@ app.get('/history', async (req, res) => {
         const { user_id } = req.user;
         if (user_id) {
             const result = await getIncomeTransaction(user_id);
+            responseService.response(req, null, result, res);
+        } else {
+            let error = { status: 400, message: "user id is required" }
+            responseService.response(req, error, null, res);    
+        }
+    } catch (error) {
+        responseService.response(req, error, null, res);
+    }
+});
+
+app.get('/down-level-income', async (req, res) => {
+    console.log(`url : ${req.protocol}://${req.hostname}:3000${req.baseUrl}${req.path}, method: ${req.method}`);
+    try {
+        const { user_id } = req.user;
+        if (user_id) {
+            const result = await getLevelIncome(user_id);
             responseService.response(req, null, result, res);
         } else {
             let error = { status: 400, message: "user id is required" }
