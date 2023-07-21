46df97b7aba840cf319d2ec8dc54685726452ce5
Vicky Pramanik <vicky.pramanik.1991@gmail.com>
2023-07-21 14:05:35 +0530

    online withdrawl update

--- admin/contoller/income/income.route.js
+++ admin/contoller/income/income.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { changeIncomeStatus,getAllIncome } = require('./income.service');
+const { changeIncomeStatus, getAllIncome, onlineWithdrawal } = require('./income.service');
 const responseService = require('../../../response/response.handler');
 const { getUserIdFromToken } = require('../../../commonHelper');
 const incomeTransactionSchema = require('../../../controller/income/transaction/incometransaction.model');
@@ -8,7 +8,7 @@ const incomeTransactionSchema = require('../../../controller/income/transaction/
 app.post('/changestatus',(req,res) => {
     try{
         console.log(`url : ${req.protocol}://${req.hostname}:3000${req.baseUrl}${req.path}`);
-        changeIncomeStatus(getUserIdFromToken(req.headers.token),req.body.transactionId,req.body.status).then((result) => {
+        changeIncomeStatus(getUserIdFromToken(req.headers.token),req.body.transactionId, req.body.status).then((result) => {
             responseService.response(req, null, result, res);
         }).catch((err) => {
             responseService.response(req, err, null, res);
@@ -32,27 +32,16 @@ app.get('/:status',(req,res) => {
 });
 
 app.post("/pay-online", async (req, res) => {
-  const mockBodyData = {
-    id: "64b5755903bb5b1cbea829e1",
-    transactionId: "23232323",
-  };
-    const body = mockBodyData;
-    console.log(body)
-  // find the document in DB for withdrawal request document
-    const data = await incomeTransactionSchema.findOneAndUpdate(
-      { _id: body.id },
-      {
-        $set: {
-          payment_type: "ONLINE",
-          status: "APPROVED",
-          bank_ref_no: "23242",
-          client_id: "242323",
-          transaction_id: "23242",
-          paid_amount: 240,
-        },
-      }
-    );
-  res.send(data);
+  try {
+    const { transactionId, status, payment_details } = req.body;
+    console.log(body);
+    const { user_id } = req.user;
+    const result = await onlineWithdrawal(user_id, transactionId, status, payment_details);
+    responseService.response(req, null, result, res);
+  } catch (error) {
+    console.log('PAY_ONLINE_ERROR : ', error);
+    responseService.response(req, error, null, res);
+  }
 });
 
 module.exports.income = app;
