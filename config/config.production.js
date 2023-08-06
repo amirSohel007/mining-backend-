90a1566d58f61cbd85d6672a455816290ad4536b
Amir Sohel <asohel@starbucks.com>
2023-08-06 16:26:27 +0530

    update the db strings

--- config/config.production.js
+++ config/config.production.js
@@ -1,17 +1,17 @@
 module.exports = {
   db: {
     mongo:
-      "mongodb+srv://admin:admin@cluster0.q9jsz.mongodb.net/?retryWrites=true&w=majority/mining",
+      "mongodb+srv://admin:admin@cluster0.lhwqg4z.mongodb.net/?retryWrites=true&w=majority",
   },
   allowedOrigins: ["*"],
   jwtSecretKey: "ITo7zN@7WBD7QmLprJudmE#4!M6*NI",
   jwtExpiresIn: "7d",
   s3Bucket: {
-    bucket_name: 'mining-project',
-    access_key: 'AKIAYMTDEK7Y5NDVRF7N',
-    secret_access_key: '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',
-    signature_version: 'AWS4-HMAC-SHA256',
-    region: 'us-east-2',
+    bucket_name: "mining-project",
+    access_key: "AKIAYMTDEK7Y5NDVRF7N",
+    secret_access_key: "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",
+    signature_version: "AWS4-HMAC-SHA256",
+    region: "us-east-2",
   },
-  useS3: false
+  useS3: false,
 };
