5c8ac15e719475cedfe3198d9799fe2eeafc5f42
Amir Sohel <asohel@starbucks.com>
2023-09-21 12:37:24 +0530

    payout api added

--- controller/fund/fund.route.js
+++ controller/fund/fund.route.js
@@ -136,7 +136,7 @@ app.post("/payout", async (req, res) => {
         SPKey: "NEFT",
       },
     });
-    res.send(data);
+    res.send(data.data);
   } catch (error) {
       console.log(error)
   }
